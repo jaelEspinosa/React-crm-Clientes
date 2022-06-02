2eba304ae7812b64ecfadf12cfa83a0a83971d9e
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-06-03 00:05:34 +0200

    error correct

--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import Spinner from '../components/spinner'
+import Spinner from '../components/Spinner'
 
 const VerCliente = () => {
     const { id } = useParams()
